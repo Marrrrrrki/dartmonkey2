31bb8a98793a5cb1b8257d86a3a326dc6c07307a
Marrrrrrki <f_mcguirk@icloud.com>
2026-08-23 18:21:10 +1000

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace DartMonkey2;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "55.0";
+    public const string Version = "1.0.0";
+    public const string Name = "DartMonkey2";
+
+    public const string Description = "An empty mod";
+
+    public const string RepoOwner = "GitHubUsername"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "DartMonkey2"; // TODO add your repo name here, also in the download url in README.md
+}
